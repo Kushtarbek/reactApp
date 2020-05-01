3fd6a1f7857160c7d73b10473b3584b31c6f33e9
Kushtar <44345257+Kushtarbek@users.noreply.github.com>
2020-05-01 00:27:59 -0500

    Commit 2

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
-const welcome = {
-   greeting: 'Hey',
-   title : "My Title",
-   
-}
+const list = [{
+  title: 'React',
+  url: 'https://reactjs.org/', 
+  author: 'Jordan Walke', 
+  num_comments: 3, 
+  points: 4, 
+  objectID: 0, 
+},
+   { title: 'Redux',
+    url: 'https://redux.js.org/', 
+    author: 'Dan Abramov, Andrew Clark', 
+    num_comments: 2, 
+    points: 5, 
+    objectID: 1, }, 
+];
 
 
 function App() {
@@ -16,13 +25,23 @@ function App() {
     <div className="App">
       <header className="App-header">
        
-        <h1>{welcome.greeting} {welcome.title}
-        
-          
-          <p>Learn React Kushu, learn it!</p>
-        </h1>
+       <h1>My hacker stories</h1>
+
         <label htmlFor="search">Search: </label> 
         <input id="search" type="text" />
+
+        <hr />
+        {list.map(function(item){
+          return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            </div>);
+        })}
       </header>
     </div>
   );
