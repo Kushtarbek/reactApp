80ec94cabde4cb1949d5d12491e500d0615edfe5
Kushtar <44345257+Kushtarbek@users.noreply.github.com>
2020-05-06 18:24:01 -0500

    states added

--- App.js
+++ App.js
@@ -2,58 +2,68 @@ import React from 'react';
 import './App.css';
 
 
-const list = [{
-  title: 'React',
-  url: 'https://reactjs.org/', 
-  author: 'Jordan Walke', 
-  num_comments: 3, 
-  points: 4, 
-  objectID: 0, 
-},
-   { title: 'Redux',
-    url: 'https://redux.js.org/', 
-    author: 'Dan Abramov, Andrew Clark', 
-    num_comments: 2, 
-    points: 5, 
-    objectID: 1, }, 
-];
 
 
-function App() {
+
+ const App = () => {
+   const stories = [
+    {
+      title: ' React ',
+      url: 'https://reactjs.org/', 
+      author: 'Jordan Walke ', 
+      num_comments: 3, 
+      points: 4, 
+      objectID: 0, 
+    },
+       { title: ' Redux ',
+        url: 'https://redux.js.org/', 
+        author: 'Dan Abramov, Andrew Clark ', 
+        num_comments: 2, 
+        points: 5, 
+        objectID: 1, },
+    ];
   
   return (
-    <div className="App">
-      <header className="App-header">
-       
-       <h1>My hacker stories</h1>
+       <div>
+          <h1>My hacker stories</h1>
 
-       
+          <Search/> 
 
-        <hr />
-        <List/>
-        <Search/>
-        <p>some text</p>
-        <button> some button</button>
-        <List/>
-        <p>some text</p>
-        <button> some button</button>
-        <p color="red">some text</p>
-        <button > my button</button>
-        <p>some text</p>
-        <button> some button</button>
-        <p>some text</p>
-        <button> some button</button>
-        <List/>
-        
-
-      </header>
-    </div>
+          <hr />
+          <List list={stories}/>
+       </div>
   );
-}
+};
 
-function List(){
-  return list.map(function(item){
-    return (
+/*Search Component is extracted to handle state & show state without revealing its content.*/
+const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState(''); //array is destructured to pass as initial state
+   
+
+    /*Callback function introduced*/
+    const handleChange = event => {
+
+      /*used elsewhere, but 'calls back to where it was introduced*/
+      setSearchTerm(event.target.value); 
+      //console.log(event.target.value);
+  };
+
+  return (
+    <div> 
+      <label htmlFor="search">Search: </label> 
+      <input id="search" type="text" onChange={handleChange} />
+      <p> 
+        Searching for <strong>{searchTerm}</strong>. 
+      </p> 
+    </div> 
+  )
+  };
+
+
+
+const List = props =>
+  props.list.map(item =>(
+    
     <div key={item.objectID}>
       <span>
         <a href= {item.url}>{item.title}</a>
@@ -62,29 +72,7 @@ function List(){
       <span>{item.num_comments}</span>
       <span>{item.points}</span>
       </div> 
-    )
-  })
-}
+    ));
 
-
-
-function Search(){
-  return (
-    <div>
-    <label htmlFor="search">Component Search: </label> 
-    <input id="search" type="text" />
-    </div>
-  );
-}
-
-function tick(){
-  const elem = (
-  <div>
-    <h1>Hello, world!</h1>
-    <h2>It is {new Date().toLocaleDateString()}.</h2>
-  </div>
-  );
-}
-setInterval(tick, 1000);
 
 export default App;
